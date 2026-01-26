0314459121d08b895e3721ff6aacd2fb2afe1dda
Johnson Prasad <jtprasad@ymail.com>
2026-01-26 16:02:29 -0700

    updated ProductList and CartSlice

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,13 +7,23 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
-    
+      const { name, image, cost } = action.payload;                                                       // destructure prod details from action payload
+      const existingItem = state.items.find(item => item.name === name);                                  // compare items to check if item exists in cart
+      if (exisitingItem) {                                                                                // increase quantity if item exists
+        exisitingItem.quantity++;
+      } else {                                                                                            // add to cart with 1 quantity if it does not exist
+        state.items.push({ name, image, cost, quantity: 1 });
+      }
     },
     removeItem: (state, action) => {
+      state.items = state.items.filter(item => item.name !== action.payload);
     },
-    updateQuantity: (state, action) => {
-
-    
+    updateQuantity: (state, action) => {                                                                  // destructure name and new quant from action payload
+      const { name, quantity } = action.payload;
+      const itemToUpdate = state.items.find(item => item.name === name);                                  // find item in cart matching given name
+      if (itemToUpdate) {
+        itemToUpdate.quantity = quantity;                                                                 // update quant to ne value if item is found
+      }
     },
   },
 });
