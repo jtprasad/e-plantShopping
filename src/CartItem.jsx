d0ffac413df04fae3883162d0177ea5436061872
Johnson Prasad <jtprasad@ymail.com>
2026-01-26 16:34:28 -0700

    updated CartItem

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,27 +9,52 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    let total = 0
+    cart.forEach(item => {
+      const quantity = item.quantity;
+      const cost = item.cost;
+      const price = parseFloat(cost.substring(1));
+      const itemTotal = price * quantity;
+      total += itemTotal;
+    });
+    return total;
   };
 
   const handleContinueShopping = (e) => {
-   
+   onContinueShopping(e);
   };
 
-
+  const handleCheckoutShopping = (e) => {
+    alert('Functionality to be added for furture reference');
+  };
 
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({
+      name: item.name,
+      quantity: item.quantity + 1
+    }));
   };
 
   const handleDecrement = (item) => {
-   
+    if (item.quantity > 1) {
+      dispatch(updateQuantity({
+        name: item.name,
+        quantity: item.quantity - 1
+      }));
+    } else {
+      dispatch(removeItem(item.name));
+    }
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item.name));
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    const price = parseFloat(item.cost.substring(1));
+    const totalCost = price * item.quantity;
+    return totalCost;
   };
 
   return (
