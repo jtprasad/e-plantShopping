5a7ad6438727c326b347fdf96ff3e4fed1efa8ef
Johnson Prasad <jtprasad@ymail.com>
2026-01-27 01:11:29 -0700

    updated cart numbers and addtocart btn

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import './ProductList.css'
 import CartItem from './CartItem';
 import { addItem } from './CartSlice';
@@ -260,9 +260,14 @@ function ProductList({ onHomeClick }) {
 
     const addToCart = (product) => {
         dispatch(addItem(product));
-        setAddedToCart((prevState) => ({
-            ...prevState, [product.name]: true,
-        }));
+    };
+
+    const CartItems = useSelector(state => state.cart.items);
+    const totalQuantity = CartItems.reduce((total, item) => total + item.quantity, 0);
+    const cart = useSelector(state => state.cart.items);
+
+    const calculateTotalQuantity = () => {
+        return CartItems ? CartItems.reduce((total, item) => total + item.quantity, 0) : 0;
     };
     return (
         <div>
@@ -281,7 +286,19 @@ function ProductList({ onHomeClick }) {
                 </div>
                 <div style={styleObjUl}>
                     <div> <a href="#" onClick={(e) => handlePlantsClick(e)} style={styleA}>Plants</a></div>
-                    <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg></h1></a></div>
+                    <div> 
+                        <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}>
+                            <h1 className='cart'>
+                                <span className="cart_quantity_count">{totalQuantity}</span>
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68">
+                                <rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle>
+                                <circle cx="184" cy="216" r="12"></circle>
+                                <path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" 
+                                    fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path>
+                                </svg>
+                            </h1>
+                        </a>
+                    </div>
                 </div>
             </div>
             {!showCart ? (
@@ -299,10 +316,10 @@ function ProductList({ onHomeClick }) {
                                         <div className='product-description'>{plant.description}</div>
                                         <div className='product-cost'>{plant.cost}</div>
                                         <button className='product-button' 
-                                            disabled={addedToCart[plant.name]}
+                                            disabled={cart.some(item => item.name === plant.name)}
                                             onClick={() => addToCart(plant)}
-                                            style={{backgroundColor: addedToCart[plant.name] ? 'gray': ''}}>
-                                                {addedToCart[plant.name] ? "Added to Cart" : "Add to Cart"}</button>
+                                            style={{backgroundColor: cart.some(item => item.name === plant.name) ? 'gray': ''}}>
+                                                {cart.some(item => item.name === plant.name) ? "Added to Cart" : "Add to Cart"}</button>
                                     </div>
                                 ))}
                             </div>
